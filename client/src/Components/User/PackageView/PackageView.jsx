e01daf714041171504dccd300a872c4a7e1a87f0
hafizswabah <swabah.a.avd121@gmail.com>
2023-07-06 17:14:02 +0530

    product view page Updated with steper component implemented

--- client/src/Components/User/PackageView/PackageView.jsx
+++ client/src/Components/User/PackageView/PackageView.jsx
@@ -6,21 +6,78 @@ import { baseImgUrl } from '../../../urls'
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import './packageView.css'
 import Rating from '@mui/material/Rating';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Input from '@mui/material/Input';
+import ImageViewer from '../ImageViewer/ImageViewer'
+import InputAdornment from '@mui/material/InputAdornment';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import StepContent from '@mui/material/StepContent';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 function PackageView() {
     const [packages, setPackages] = useState([])
     const [value, setValue] = useState('');
-    console.log(packages);
+    const [images, setImages] = useState([])
+    const steps = [
+        {
+            label: 'Trip Day 1',
+            description: `For each ad campaign that you create, you can control how much
+                    you're willing to spend on clicks and conversions, which networks
+                    and geographical locations you want your ads to show on, and more.`,
+        },
+        {
+            label: 'Trip Day 2',
+            description:
+                'An ad group contains one or more ads which target a shared set of keywords.',
+        },
+        {
+            label: 'Final Day',
+            description: `Try out different ad text to see what brings in the most customers,
+                    and learn how to enhance your ads using features like ad extensions.
+                    If you run into any problems with your ads, find out how to tell if
+                    they're running and how to resolve approval issues.`,
+        },
+    ];
+
+    const [activeStep, setActiveStep] = React.useState(0);
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+    };
+
     const { id } = useParams()
     useEffect(() => {
         (async function () {
             let { data } = await axios.get("/user/package-view/" + id)
             if (!data.err) {
                 setPackages(data.packages)
+                let subImages = data.packages.subImages.map(item => {
+                    return { label: item.name, imgPath: baseImgUrl + item.filename }
+                })
+                setImages([{ label: data.packages.name, imgPath: baseImgUrl + data.packages.mainImage[0].filename }, ...subImages])
             }
         })()
     }, [])
     const formattedStartDate = new Date(packages.startDate).toLocaleDateString();
     const formattedEndDate = new Date(packages.endDate).toLocaleDateString()
+
+
+
+
+
 
     return (
         <>
@@ -28,41 +85,60 @@ function PackageView() {
                 <div className="container">
                     <div className="row gx-5">
                         <aside className="col-lg-6">
-                            <div className="border rounded-4 mb-3 d-flex justify-content-center">
-                                <Link
-                                    to="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/detail1/big.webp"
-                                    data-fslightbox="mygalley"
-                                    className="rounded-4"
-                                    target="_blank"
-                                    data-type="image"
-                                >
-                                    {packages.mainImage &&
-                                        <img
-                                            style={{ maxWidth: '100%', maxHeight: '100vh', margin: 'auto' }}
-                                            className="rounded-4 fit"
-                                            src={baseImgUrl + packages.mainImage[0].filename}
-                                            alt="Product Image"
-                                        />
+                            <div className=" rounded-4 mb-3 d-flex justify-content-center">
 
-                                    }
-
-                                </Link>
-                            </div>
-                            <div className="d-flex justify-content-center mb-3">
-                                {packages?.subImages?.map((subImage) => (
-                                    <img
-                                        width="60"
-                                        height="60"
-                                        className="rounded-2"
-                                        src={baseImgUrl + subImage.filename}
-                                        alt="Thumbnail 1"
-                                    />
-
-                                ))
-
+                                {
+                                    images &&
+                                    <ImageViewer data={images} />
                                 }
-
                             </div>
+                            <div className="stepers m-5">
+                                <Stepper activeStep={activeStep} orientation="vertical">
+                                    {steps.map((step, index) => (
+                                        <Step key={step.label}>
+                                            <StepLabel
+                                                optional={
+                                                    index === 2 ? (
+                                                        <Typography variant="caption">Last step</Typography>
+                                                    ) : null
+                                                }
+                                            >
+                                                {step.label}
+                                            </StepLabel>
+                                            <StepContent>
+                                                <Typography style={{fontSize:"11px",fontWeight:'400'}}>{step.description}</Typography>
+                                                <Box sx={{ mb: 2 }}>
+                                                    <div>
+                                                        <Button
+                                                            variant="contained"
+                                                            onClick={handleNext}
+                                                            sx={{ mt: 1, mr: 1 }}
+                                                        >
+                                                            {index === steps.length - 1 ? 'Finish' : 'Continue'}
+                                                        </Button>
+                                                        <Button
+                                                            disabled={index === 0}
+                                                            onClick={handleBack}
+                                                            sx={{ mt: 1, mr: 1 }}
+                                                        >
+                                                            Back
+                                                        </Button>
+                                                    </div>
+                                                </Box>
+                                            </StepContent>
+                                        </Step>
+                                    ))}
+                                </Stepper>
+                                {activeStep === steps.length && (
+                                    <Paper square elevation={0} sx={{ p: 3 }}>
+                                        <Typography style={{fontSize:"14px",fontWeight:'600'}}>Complted Your Journey OverView</Typography>
+                                        <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+                                            Reset
+                                        </Button>
+                                    </Paper>
+                                )}
+                            </div>
+
                         </aside>
                         <main className="col-lg-6">
                             <div className="ps-lg-3">
@@ -79,10 +155,6 @@ function PackageView() {
                                             }}
                                         />
                                     </div>
-                                    <span className="text-muted">
-                                        <i className="fas fa-shopping-basket fa-sm mx-1"></i>154 orders
-                                    </span>
-                                    <span className="text-success ms-2">In stock</span>
                                 </div>
 
                                 <div className="mb-3">
@@ -90,59 +162,45 @@ function PackageView() {
                                     <span className="text-muted">/per head</span>
                                 </div>
 
-                                <p>{packages.description}</p>
+                                <p className='pkg-desc'>{packages.description}</p>
 
                                 <div className="row">
-                                    <dt className="col-3">Destination:</dt>
-                                    <dd className="col-9">{packages.destination}</dd>
-                                    <dt className="col-3">Date:</dt>
-                                    <dd className="col-9">{formattedStartDate} to {formattedEndDate}</dd>
+                                    <dt className="col-3 pkg-det">Destination:</dt>
+                                    <dd className="col-9 pkg-details">{packages.destination}</dd>
+                                    <dt className="col-3 pkg-det">Date:</dt>
+                                    <dd className="col-9 pkg-details">{formattedStartDate} to {formattedEndDate}</dd>
 
-                                    <dt className="col-3">Duration</dt>
-                                    <dd className="col-9">{packages.duration}</dd>
+                                    <dt className="col-3 pkg-det">Duration</dt>
+                                    <dd className="col-9 pkg-details">{packages.duration}</dd>
 
-                                    <dt className="col-3">Visit Places</dt>
-                                    <dd className="col-9">{packages.visitPlaces}</dd>
+                                    <dt className="col-3 pkg-det">Visit Places</dt>
+                                    <dd className="col-9 pkg-details">{packages.visitPlaces}</dd>
 
-                                    <dt className="col-3">Flight Bookings</dt>
-                                    <dd className="col-9">{packages.flightbooking ? 'Inluded Flight Tickets' : 'No Flight Tickets'}</dd>
-                                    <dt className="col-3">Stay Bookings</dt>
-                                    <dd className="col-9">{packages.staybooking ? 'Inluded Stay Tickets' : 'No Stay Booking Included'}</dd>
+                                    <dt className="col-3 pkg-det">Flight Bookings</dt>
+                                    <dd className="col-9 pkg-details">{packages.flightbooking ? 'Inluded Flight Tickets' : 'No Flight Tickets'}</dd>
+                                    <dt className="col-3 pkg-det">Stay Bookings</dt>
+                                    <dd className="col-9 pkg-details">{packages.staybooking ? 'Inluded Stay Tickets' : 'No Stay Booking Included'}</dd>
+                                    <dt className="col-3 pkg-det">Slots Remaining</dt>
+                                    <dd className="col-9 pkg-details">{packages.totalSlots}</dd>
                                 </div>
 
                                 <hr />
 
                                 <div className="row mb-4">
-                                    <label className="mb-2 d-block">Tickets</label>
-                                    <div className="col-md-4 col-2 mb-3 d-flex align-item-center justify-content-center">
-
-                                        <button class="Btn">
-
-                                            <div class="sign">
-                                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="90" height="90" viewBox="0 0 30 30">
-                                                    <path d="M15,3C8.373,3,3,8.373,3,15c0,6.627,5.373,12,12,12s12-5.373,12-12C27,8.373,21.627,3,15,3z M21,16h-5v5 c0,0.553-0.448,1-1,1s-1-0.447-1-1v-5H9c-0.552,0-1-0.447-1-1s0.448-1,1-1h5V9c0-0.553,0.448-1,1-1s1,0.447,1,1v5h5 c0.552,0,1,0.447,1,1S21.552,16,21,16z"></path>
-                                                </svg>
-                                            </div>
-
-                                            <div class="text">Add</div>
-                                        </button>
-
-                                    </div>
-                                    <div className="col-md-6">
-                                        <input type="text" className='mt-2' />
-                                    </div>
-                                    <div className="col-md-2 col-2 mb-3">
-
-                                        <button class="Btn">
-
-                                            <div class="sign">
-                                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="90" height="90" viewBox="0 0 30 30">
-                                                    <path d="M15,3C8.373,3,3,8.373,3,15c0,6.627,5.373,12,12,12s12-5.373,12-12C27,8.373,21.627,3,15,3z M21,16h-5v5 c0,0.553-0.448,1-1,1s-1-0.447-1-1v-5H9c-0.552,0-1-0.447-1-1s0.448-1,1-1h5V9c0-0.553,0.448-1,1-1s1,0.447,1,1v5h5 c0.552,0,1,0.447,1,1S21.552,16,21,16z"></path>
-                                                </svg>
-                                            </div>
-
-                                            <div class="text">minus</div>
-                                        </button>
+                                    <div className=" mb-3 ">
+                                        <p>Tickets</p>
+                                        <FormControl variant="standard" sx={{ m: 1, mt: 3, width: '25ch' }} style={{ margin: "0px" }}>
+                                            <Input
+                                                id="standard-adornment-weight"
+                                                endAdornment={<InputAdornment position="end"></InputAdornment>}
+                                                aria-describedby="standard-weight-helper-text"
+                                                inputProps={{
+                                                    'aria-label': 'How many tickets would you like?',
+                                                }}
+                                                type='number'
+                                            />
+                                            <FormHelperText id="standard-weight-helper-text">How many tickets would you like to purchase?</FormHelperText>
+                                        </FormControl>
 
                                     </div>
                                 </div>
