66a214d4f67e2dabf22e1f359424f43f74459812
hafizswabah <swabah.a.avd121@gmail.com>
2023-07-25 22:07:40 +0530

    edit profile set up

--- client/src/Components/User/Profile/UserProfile.jsx
+++ client/src/Components/User/Profile/UserProfile.jsx
@@ -1,45 +1,131 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Swal from 'sweetalert2';
 import EditProfileModal from '../../../modal/EditProfile';
 import NavBar from '../NavBar/NavBar';
 import './UserProfile.css'
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 const UserProfile = () => {
 
-  const [showModal,setShowModal]=useState(false)
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+  const [state, setState] = React.useState({
+
+    vertical: 'bottom',
+    horizontal: 'center',
+  });
+  const { vertical, horizontal } = state;
+  const [showModal, setShowModal] = useState(false)
+  const [reload, setReload] = useState(false)
+  const [bookingList, setBookingList] = useState([])
+  const [planBookingList, setPlanBookings] = useState([])
+  const [open, setOpen] = useState(false);
   const { user } = useSelector((state) => {
     return state
   })
+  function getPackageCount() {
+    return bookingList.length
+  }
+  function getTicketCount() {
+    return planBookingList.length
+  }
+  const PackagesCount = getPackageCount()
+  const Tickets = getTicketCount()
+  function completedTrips() {
+    const currentDate = new Date()
+    let completedTrip = bookingList.filter((item) => {
+      return item.status === 'upcoming' && new Date(item?.PackageId.endDate) > currentDate
+    })
+    return completedTrip.length
+  }
+  function upcomingTrips() {
+    const currentDate = new Date()
+    let completedTrip = bookingList.filter((item) => {
+      return item.status === 'upcoming' && new Date(item?.PackageId.endDate) < currentDate
+    })
+    return completedTrip.length
+  }
+  let upcomingTripCount = upcomingTrips()
+  function getupcomingTickets() {
+    let currentDate = new Date()
+    let completedTicket = planBookingList.filter((item) => {
+      return item.status === 'upcoming' && new Date(item?.PlanId.date) < currentDate
+    })
+    console.log(completedTicket, 'hhh');
+    return completedTicket.length
+  }
+  let upcomingevent = getupcomingTickets()
+  let completedTripCount = completedTrips()
+  function getCompletedTickets() {
+    let currentDate = new Date()
+    let completedTicket = planBookingList.filter((item) => {
+      return item.status === 'upcoming' && new Date(item?.PlanId.date) > currentDate
+    })
+    return completedTicket.length
+  }
+  let completedTicketCount = getCompletedTickets()
+
+  let upcomings = upcomingevent + upcomingTripCount
+  console.log(upcomingevent);
+
+  let completed = completedTicketCount + completedTripCount
 
   const userId = user.details._id
   async function logout() {
     Swal.fire({
-        title: 'Do you want to logout',
-        text: "Are you sure ?",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: 'red',
-        cancelButtonColor: '##a8a8a8',
-        confirmButtonText: 'Yes,Logout!'
+      title: 'Do you want to logout',
+      text: "Are you sure ?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: 'red',
+      cancelButtonColor: '##a8a8a8',
+      confirmButtonText: 'Yes,Logout!'
     }).then(async (result) => {
-        if (result.isConfirmed) {
+      if (result.isConfirmed) {
 
-            await axios.get("/user/auth/logout");
-            dispatch({ type: "refresh" })
-        }
+        await axios.get("/user/auth/logout");
+        dispatch({ type: "refresh" })
+      }
     })
-}
-const handleModel=()=>{
-  setShowModal(true)
-}
-const handleCloseModal=()=>{
-setShowModal(false)
-}
+  }
+  const handleCloseSnackBar = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const handleModel = () => {
+    setShowModal(true)
+  }
+  const handleCloseModal = () => {
+    setShowModal(false)
+    setReload(!reload)
+    setOpen(true)
+  }
+  useEffect(() => {
+    (async function () {
+      const { data } = await axios.get("/user/booking/" + userId);
+      console.log(data);
+      if (!data.err) {
+        setBookingList(data.bookings)
+        setPlanBookings(data.PlanBookings)
+      }
+    })()
+  }, [reload])
   return (
     <div>
       <NavBar />
-      <EditProfileModal showModal={showModal} handleCloseModal={handleCloseModal}/>
+      <Snackbar open={open} autoHideDuration={2000} onClose={handleCloseSnackBar}  key={vertical + horizontal} anchorOrigin={{ vertical, horizontal }}>
+        <Alert severity="success" sx={{ width: '100%' }}>
+          Successfully updated
+        </Alert>
+      </Snackbar>
+      <EditProfileModal showModal={showModal} handleCloseModal={handleCloseModal} />
       <section className="section about-section gray-bg" id="about">
         <div className="container">
           <div className="row align-items-center flex-row-reverse">
@@ -72,7 +158,7 @@ setShowModal(false)
                   </div>
                   <div className="col-3 d-flex align-items-center justify-content-center mt-4 m-1">
                     <button className='log-btn' onClick={handleModel}>
-                     EDIT PROFILE
+                      EDIT PROFILE
                     </button>
                   </div>
                 </div>
@@ -88,25 +174,25 @@ setShowModal(false)
             <div className="row">
               <div className="col-6 col-lg-3">
                 <div className="count-data text-center">
-                  <h6 className="count h2" data-to="500" data-speed="500">500</h6>
+                  <h6 className="count h2" data-to="500" data-speed="500">{PackagesCount}</h6>
                   <p className="m-0px font-w-600">Tour Package</p>
                 </div>
               </div>
               <div className="col-6 col-lg-3">
                 <div className="count-data text-center">
-                  <h6 className="count h2" data-to="150" data-speed="150">150</h6>
+                  <h6 className="count h2" data-to="150" data-speed="150">{Tickets}</h6>
                   <p className="m-0px font-w-600">Tickets</p>
                 </div>
               </div>
               <div className="col-6 col-lg-3">
                 <div className="count-data text-center">
-                  <h6 className="count h2" data-to="850" data-speed="850">850</h6>
+                  <h6 className="count h2" data-to="850" data-speed="850">{completed}</h6>
                   <p className="m-0px font-w-600">Completed</p>
                 </div>
               </div>
               <div className="col-6 col-lg-3">
                 <div className="count-data text-center">
-                  <h6 className="count h2" data-to="190" data-speed="190">190</h6>
+                  <h6 className="count h2" data-to="190" data-speed="190">{upcomings}</h6>
                   <p className="m-0px font-w-600">Upcoming Events</p>
                 </div>
               </div>
--- client/src/modal/EditProfile.jsx
+++ client/src/modal/EditProfile.jsx
@@ -3,21 +3,19 @@ import React, { useEffect, useState } from 'react';
 import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert from '@mui/material/Alert';
- 
 
-const EditProfileModal = ({ showModal, handleCloseModal}) => {
-    const Alert = React.forwardRef(function Alert(props, ref) {
-        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-      });
-     
-    
+
+
+const EditProfileModal = ({ showModal, handleCloseModal }) => {
+  
+
+
     const [userName, setUserName] = useState('')
     const [_id, setId] = useState(null)
     const [userEmail, setEmail] = useState('')
-    const [userContact, setContact] = useState('')
-    const [open, setOpen] =useState(false);
+    const [userContact, setContact] = useState(0)
+
+
 
     const { user } = useSelector((state) => {
         return state
@@ -35,7 +33,8 @@ const EditProfileModal = ({ showModal, handleCloseModal}) => {
             userName, userEmail, userContact, _id
         })
         if (!data.err) {
-            setOpen(true);
+       
+            handleCloseModal()
         }
     }
     return (
@@ -58,7 +57,7 @@ const EditProfileModal = ({ showModal, handleCloseModal}) => {
                         <Col>
                             <Form.Group className="mb-3" controlId="slots">
 
-                                <Form.Control type="number" placeholder='Contact' style={{ width: '100%' }} defaultValue={user.details.contact} onChange={(e) => { setEmail(e.target.value) }} />
+                                <Form.Control type="number" placeholder='Contact' style={{ width: '100%' }} defaultValue={user.details.contact} onChange={(e) => { setContact(e.target.value) }} />
                             </Form.Group>
                         </Col>
                     </Row>
@@ -66,7 +65,7 @@ const EditProfileModal = ({ showModal, handleCloseModal}) => {
                         <Col>
                             <Form.Group className="mb-3" controlId="slots">
 
-                                <Form.Control type="email" placeholder='Email' style={{ width: '100%' }} defaultValue={user.details.email} onChange={(e) => { setContact(e.target.value) }} />
+                                <Form.Control type="email" placeholder='Email' style={{ width: '100%' }} defaultValue={user.details.email} onChange={(e) => { setEmail(e.target.value) }} />
                             </Form.Group>
                         </Col>
                     </Row>
@@ -80,11 +79,7 @@ const EditProfileModal = ({ showModal, handleCloseModal}) => {
                     Edit Profile
                 </Button>
             </Modal.Footer>
-            <Snackbar open={open} autoHideDuration={1000} >
-                <Alert  severity="success" sx={{ width: '100%' }}>
-                    This is a success message!
-                </Alert>
-            </Snackbar>
+      
         </Modal>
     );
 };
