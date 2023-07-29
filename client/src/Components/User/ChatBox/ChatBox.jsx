714709ea9cf48caf0a2acb59f4d65c89d0e87b11
hafizswabah <swabah.a.avd121@gmail.com>
2023-07-29 14:07:25 +0530

    chat box added

--- client/src/Components/User/ChatBox/ChatBox.jsx
+++ client/src/Components/User/ChatBox/ChatBox.jsx
@@ -3,12 +3,15 @@ import React, { useEffect, useState } from 'react'
 import { Col, Row } from 'react-bootstrap';
 import defaultChatImg from '../../../assets/defaultChatImg.jpg'
 import './chatbox.css'
-import {format} from "timeago.js"
+import { format } from "timeago.js"
+import InputEmoji from 'react-input-emoji'
+import { Button } from '@mui/material';
 
 function ChatBox({ chat, currentUserId }) {
   const [userData, setUserData] = useState(null)
   const [messages, setMessages] = useState([])
-console.log(currentUserId);
+  const [newMessages, setnewMessages] = useState("")
+  console.log(newMessages);
   let userId = chat?.members.find((id) => id !== currentUserId)
   useEffect(() => {
     (async function () {
@@ -36,40 +39,56 @@ console.log(currentUserId);
   return (
     <>
       <div className="ChatBox-container">
-        <>
-          <div className="chat-header">
-            <div className="chat-follower">
-              <Row>
-                <Col md={4}>
-                  <div>
-
-                    <img src={defaultChatImg} alt="" className='followerImage' style={{ width: "50px", height: "50px", borderRadius: "23px" }} />
-                    <div className="name" style={{ fontSize: "0.6rem" }}>
-
-                    </div>
-                  </div>
-                </Col>
-                <Col md={8}>
-                  <div className="chatUserDetails d-flex align-items-center">
-                    <span>{userData?.[0].name}</span>
-                  </div>
-                </Col>
-              </Row>
-            </div>
-          <hr style={{ border: "0.1px solid #cfcfcf" }} />
-          </div>
-          <div className="chat-body">
-            {messages?.map((message) => (
-              <>
-                <div className={message.senderId === currentUserId ? "message own" : "message"}>
-                  <span>{message?.text}</span>
-                  <span>{format(message?.createdAt)}</span>
-                </div>
-              </>
-            )
-            )}
-          </div>
-        </>
+        {chat ? (
+          <>
+     
+        
+             <div className="chat-header">
+               <div className="chat-follower">
+                 <Row>
+                   <Col md={4}>
+                     <div>
+   
+                       <img src={defaultChatImg} alt="" className='followerImage' style={{ width: "50px", height: "50px", borderRadius: "23px" }} />
+                       <div className="name" style={{ fontSize: "0.6rem" }}>
+   
+                       </div>
+                     </div>
+                   </Col>
+                   <Col md={8}>
+                     <div className="chatUserDetails d-flex align-items-center">
+                       <span>{userData?.[0].name}</span>
+                     </div>
+                   </Col>
+                 </Row>
+               </div>
+               <hr style={{ border: "0.1px solid #cfcfcf" }} />
+             </div>
+             <div className="chat-body">
+               {messages?.map((message) => (
+                 <>
+                   <div className={message.senderId === currentUserId ? "message own" : "message"}>
+                     <span>{message?.text}</span>
+                     <span>{format(message?.createdAt)}</span>
+                   </div>
+                 </>
+               )
+               )}
+             </div>
+    
+           <div className="chat-sender">
+             <div>+</div>
+             <InputEmoji
+               value={newMessages}
+               onChange={(newMessages) => { setnewMessages(newMessages) }}
+             ></InputEmoji>
+             <Button>send</Button>
+           </div>
+           </>
+        ):(
+          <span>hiii</span>
+        )}
+     
       </div>
     </>
   )
