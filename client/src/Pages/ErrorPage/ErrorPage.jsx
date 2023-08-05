5b26f900cdf85ab7b30f821845a82fc41a6b5bfc
hafizswabah <swabah.a.avd121@gmail.com>
2023-08-05 14:58:07 +0530

    bug fixing

--- client/src/Pages/ErrorPage/ErrorPage.jsx
+++ client/src/Pages/ErrorPage/ErrorPage.jsx
@@ -4,21 +4,35 @@ import "../css/error.css"
 
 function ErrorPage() {
   return (
-    <section class="page_404">
+ <div style={{
+
+ }}>
       <div class="container">
         <div class="row">
-          <div  class="col-sm-12 div ">
-            <div style={{display:'flex',flexDirection:"column",justifyContent:'center',alignItems:'center'}} class="col-sm-10 col-sm-offset-1  text-center">
-              <div  class="four_zero_four_bg">
-              </div>
+          <div class="col-sm-12 div ">
+            <div style={{ display: 'flex', flexDirection: "column", justifyContent: 'center', alignItems: 'center' }} class="col-sm-12 col-sm-offset-1  text-center">
+              <div
+                className="four_zero_four_bg"
+                style={{
+                  backgroundImage: "url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif)",
+                  height: "500px",
+                  width: "80%",
+                  backgroundPosition: "center",
+                }}
+              ></div>
 
               <div class="contant_box_404">
-                <h3 class="h2">Look like you're lost</h3>
+                <h3 class="h2" style={{fontSize:"30px"}}>Look like you're lost</h3>
 
                 <p>the page you are looking for not avaible!</p>
                 <p>please try again</p>
 
-                <Link to="/" class="link_404">
+                <Link to="/" class="link_404" 
+                style={{color:"white",
+                padding:"10px 20px",
+                background:"#0d6efd",
+                margin:"20px 0px",
+                display:"inline-block"}}>
                   Go to Home
                 </Link>
               </div>
@@ -26,7 +40,7 @@ function ErrorPage() {
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
 
