d0d2fdd66964c476b9ac2e7e6f3d07f16af07a68
hafizswabah <swabah.a.avd121@gmail.com>
2024-05-25 20:14:12 +0530

    add new feture edit the details of activities

--- client/src/Components/Agency/Plans/AgencyPlans.jsx
+++ client/src/Components/Agency/Plans/AgencyPlans.jsx
@@ -7,16 +7,25 @@ import '../Package/agencypackage.css';
 import { RiMore2Fill } from 'react-icons/ri';
 import axios from 'axios';
 import Swal from 'sweetalert2';
+import EditPlanModal from '../../../modal/EditPlanModal';
 
 function AgencyPlans() {
   const [showModal, setShowModal] = useState(false);
   const [clicked, setClicked] = useState(false);
   const [plans, setPlans] = useState([]);
-  const[reload,setReload]=useState(false)
-  const[load,setLoad]=useState(false)
-const handleplanAdded=()=>{
-  setReload(!reload)
-}
+  const [reload, setReload] = useState(false)
+  const [load, setLoad] = useState(false)
+  const [editPlan, setEditPlan] = useState(false)
+  const [showEditModal, setShowEditModal] = useState(false);
+
+  function edit(item) {
+    setEditPlan(item);
+    setShowEditModal(true);
+  }
+
+  const handleplanAdded = () => {
+    setReload(!reload)
+  }
   const handleClick = () => {
     setClicked(!clicked);
   };
@@ -28,108 +37,112 @@ const handleplanAdded=()=>{
   const handleCloseModal = () => {
     setShowModal(false);
   };
-async function activate(e,id){
-  e.preventDefault()
-  Swal.fire({
-    title: 'Activate the Plan',
-    text: "Do you want to activate the plan",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: 'green',
-    cancelButtonColor: '##a8a8a8',
-    confirmButtonText: 'Yes,Activate'
-  }).then(async (result) => {
-    if (result.isConfirmed) {
-      setLoad(true)
-      const { data } = await axios.post("/agency/active-plan", {id });
-      if (!data.err) {
-        Swal.fire(
-          'Success!',
-          'Activated Plan succesfully',
-          'success'
-        )
-      } else {
-        Swal.fire(
-          'Failed!',
-          'Something Went Wrong',
-          'error'
-        )
 
+  const handleCloseEditModal = () => {
+    setShowEditModal(false);
+  };
+  async function activate(e, id) {
+    e.preventDefault()
+    Swal.fire({
+      title: 'Activate the Plan',
+      text: "Do you want to activate the plan",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: 'green',
+      cancelButtonColor: '##a8a8a8',
+      confirmButtonText: 'Yes,Activate'
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        setLoad(true)
+        const { data } = await axios.post("/agency/active-plan", { id });
+        if (!data.err) {
+          Swal.fire(
+            'Success!',
+            'Activated Plan succesfully',
+            'success'
+          )
+        } else {
+          Swal.fire(
+            'Failed!',
+            'Something Went Wrong',
+            'error'
+          )
+
+        }
+
+        setLoad(false)
+        setReload(!reload)
       }
-    
-      setLoad(false)
-      setReload(!reload)
-    }
-  })
-}
-async function Deactivate(e,id){
-  e.preventDefault()
-  Swal.fire({
-    title: 'De-Activate the Plan',
-    text: "Do you want to De-Activate the plan",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: 'red',
-    cancelButtonColor: '##a8a8a8',
-    confirmButtonText: 'Yes,De-Activate'
-  }).then(async (result) => {
-    if (result.isConfirmed) {
-      setLoad(true)
-      const { data } = await axios.post("/agency/deactivate-plan", {id });
-      if (!data.err) {
-        Swal.fire(
-          'Success!',
-          'De-Activated Plan succesfully',
-          'success'
-        )
-      } else {
-        Swal.fire(
-          'Failed!',
-          'Something Went Wrong',
-          'error'
-        )
+    })
+  }
+  async function Deactivate(e, id) {
+    e.preventDefault()
+    Swal.fire({
+      title: 'De-Activate the Plan',
+      text: "Do you want to De-Activate the plan",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: 'red',
+      cancelButtonColor: '##a8a8a8',
+      confirmButtonText: 'Yes,De-Activate'
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        setLoad(true)
+        const { data } = await axios.post("/agency/deactivate-plan", { id });
+        if (!data.err) {
+          Swal.fire(
+            'Success!',
+            'De-Activated Plan succesfully',
+            'success'
+          )
+        } else {
+          Swal.fire(
+            'Failed!',
+            'Something Went Wrong',
+            'error'
+          )
 
+        }
+
+        setLoad(false)
+        setReload(!reload)
       }
-    
-      setLoad(false)
-      setReload(!reload)
-    }
-  })
-}
-async function handleDelete(e,id) {
-  e.preventDefault()
-  Swal.fire({
-    title: 'Delete the plan',
-    text: "Do you want to Delete the plan",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: 'red',
-    cancelButtonColor: '##a8a8a8',
-    confirmButtonText: 'Yes,Delete'
-  }).then(async (result) => {
-    if (result.isConfirmed) {
-      setLoad(true)
-      const { data } = await axios.post("/agency/delete-plan", { id });
-      if (!data.err) {
-        Swal.fire(
-          'Success!',
-          'Deleted plan succesfully',
-          'success'
-        )
-      } else {
-        Swal.fire(
-          'Failed!',
-          'Something Went Wrong',
-          'error'
-        )
+    })
+  }
+  async function handleDelete(e, id) {
+    e.preventDefault()
+    Swal.fire({
+      title: 'Delete the plan',
+      text: "Do you want to Delete the plan",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: 'red',
+      cancelButtonColor: '##a8a8a8',
+      confirmButtonText: 'Yes,Delete'
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        setLoad(true)
+        const { data } = await axios.post("/agency/delete-plan", { id });
+        if (!data.err) {
+          Swal.fire(
+            'Success!',
+            'Deleted plan succesfully',
+            'success'
+          )
+        } else {
+          Swal.fire(
+            'Failed!',
+            'Something Went Wrong',
+            'error'
+          )
 
+        }
+
+        setLoad(false)
+        setReload(!reload)
       }
-
-      setLoad(false)
-      setReload(!reload)
-    }
-  })
-}
+    })
+  }
   useEffect(() => {
     (async function () {
       let { data } = await axios.get("/agency/get-plans");
@@ -144,7 +157,7 @@ async function handleDelete(e,id) {
     <div>
       <AgencyHeader handleClick={handleClick} />
       <Row className='m-0'>
-      <Col md={3} style={{padding:"0px"}}>
+        <Col md={3} style={{ padding: "0px" }}>
           <AgencySidebar page={'plans'} clicked={clicked} />
         </Col>
         <Col md={9}>
@@ -156,12 +169,12 @@ async function handleDelete(e,id) {
                 </Button>
               </div>
             </Container>
-            <AddPlanModal showModal={showModal} handleCloseModal={handleCloseModal} handleplanAdded={handleplanAdded}/>
+            <AddPlanModal showModal={showModal} handleCloseModal={handleCloseModal} handleplanAdded={handleplanAdded} />
           </Row>
           <Row>
             <div className='admin-container'>
               <h5 className='p-1' style={{ fontSize: '22px', fontWeight: 300 }}>Plans</h5>
-              <Table className='table-main' responsive size="sm" style={{fontSize:".8rem"}}>
+              <Table className='table-main' responsive size="sm" style={{ fontSize: ".8rem" }}>
                 <thead>
                   <tr>
                     <th>#</th>
@@ -178,7 +191,7 @@ async function handleDelete(e,id) {
                   {plans.map((item, index) => {
                     const statusColour = item.active ? 'green' : 'red';
                     const PlanDate = new Date(item.date).toLocaleDateString();
-                   
+
 
                     return (
                       <tr key={index}>
@@ -199,13 +212,16 @@ async function handleDelete(e,id) {
                             <Dropdown.Menu>
                               {item.active ? (
                                 <>
-                                <Dropdown.Item href='#' onClick={(e) => Deactivate(e, item._id)}>
-                                  Deactivate
-                                </Dropdown.Item>
-                                 <Dropdown.Item href='#' onClick={(e) => handleDelete(e, item._id)}>
-                                 Delete
-                               </Dropdown.Item>
-                              </>
+                                  <Dropdown.Item href='#' onClick={(e) => edit(item)}>
+                                    Edit
+                                  </Dropdown.Item>
+                                  <Dropdown.Item href='#' onClick={(e) => Deactivate(e, item._id)}>
+                                    Deactivate
+                                  </Dropdown.Item>
+                                  <Dropdown.Item href='#' onClick={(e) => handleDelete(e, item._id)}>
+                                    Delete
+                                  </Dropdown.Item>
+                                </>
                               ) : (
                                 <Dropdown.Item href='#' onClick={(e) => activate(e, item._id)}>
                                   Activate
@@ -223,6 +239,7 @@ async function handleDelete(e,id) {
           </Row>
         </Col>
       </Row>
+      <EditPlanModal showEditModal={showEditModal} handleCloseEditModal={handleCloseEditModal} editPlan={editPlan}/>
     </div>
   );
 }
---
+++ client/src/modal/EditPlanModal.jsx
@@ -0,0 +1,214 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
+import formatDate from '../helper/formatDate';
+
+
+const EditPlanModal = ({ showEditModal, handleCloseEditModal, editPlan ,handlePackageAdded }) => {
+  const [state, setState] = useState({})
+  const [startDate, setStartDate] = useState('');
+  const [description, setDescription] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [name, setName] = useState('');
+  const [flightBooking, setFlightBooking] = useState(false)
+  const [stayBooking, setStayBooking] = useState(false)
+  const [mainImage, setMainImage] = useState(null);
+  const [subImages, setSubImages] = useState([]);
+
+ 
+//   useEffect(() => {
+//     if (editpkg) {
+//       setStartDate(formatDate(editpkg.startDate))
+//       setEndDate(formatDate(editpkg.endDate))
+//       setState(editpkg)
+//     }
+//   }, [editpkg])
+
+
+  async function editPackage(e) {
+    //   e.preventDefault();
+    // const formData = new FormData();
+    // formData.append('mainImage', mainImage);
+    // for (let i = 0; i < subImages.length; i++) {
+    //     formData.append('subImages', subImages[i]);
+    // }
+    // const packageData = {
+    //   ...state, 
+    // };
+
+    // formData.append('state', JSON.stringify(packageData));
+    // const {data} = await axios.post("/agency/edit-packages", formData, {
+    //     headers: {
+    //         'Content-Type': 'multipart/form-data',
+    //     },
+    // });
+    // if(!data.err){
+    //   handleCloseEditModal()
+    //   handlePackageAdded ()
+    // }
+}
+// console.log(state,"state");
+
+  return (
+    <Modal show={showEditModal} onHide={handleCloseEditModal} centered dialogClassName="modal-md">
+      <Modal.Header closeButton>
+        <Modal.Title>Edit Your Tour Plan</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+        <Form>
+          {/* <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="slots">
+                <Form.Control type="text" style={{ width: '100%' }} placeholder='Programme Name' value={name} onChange={(e) => { setName(e.target.value) }} />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Form.Group className="mb-3" controlId="plans">
+              <Form.Select className="form-select-sm" placeholder='Type' style={{ width: '100%' }} value={category} onChange={(e) => { setCategory(e.target.value) }}>
+                <option value="Dj Night">Dj Night</option>
+                <option value="Desert">Desert Safari</option>
+                <option value="Sky Dive">Sky Dive</option>
+                <option value="Sea Drive">Sea Drive</option>
+                <option value="Trucking">Trucking</option>
+                <option value="Camp fire">camp Fire</option>
+              </Form.Select>
+            </Form.Group>
+          </Row>
+          <Row>
+            <Col>
+              <MapSearchBox setDestination={setLocation}></MapSearchBox>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="slots">
+                <Form.Control type="date" style={{ width: '100%' }} placeholder='Date' value={date} onChange={(e) => { setDate(e.target.value) }} />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="Time">
+                <Form.Control type="text" style={{ width: '100%' }} placeholder='Time' value={time} onChange={(e) => { setTime(e.target.value) }} />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="slots">
+                <Form.Control type="number" style={{ width: '100%' }} placeholder='Total Slots' value={totalSlots} onChange={(e) => { setTotalSlot(e.target.value) }} />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="slots">
+                <Form.Control type="number" style={{ width: '100%' }} placeholder='Per Head Cost' value={cost} onChange={(e) => { setCost(e.target.value) }} />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="description">
+                <Form.Control as="textarea" rows={3} placeholder="Description" style={{ width: '100%' }} value={description} onChange={(e) => { setDescription(e.target.value) }} />
+              </Form.Group>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="image">
+                <Form.Label className="form-label-sm">Event Poster Image</Form.Label>
+                <Form.Control type="file" accept='image/*' style={{ width: '100%' }} onChange={(e) => { setMainImage(e.target.files[0]) }} />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="subImages">
+                <Form.Label className="form-label-sm">Event Sub Images</Form.Label>
+                <Form.Control type="file" accept='image/*' multiple style={{ width: '100%' }} onChange={(e) => { setSubImages(e.target.files) }} />
+              </Form.Group>
+            </Col>
+          </Row> */}
+          {/* {
+            programmeDetails.map((programme, index) => (
+              <div key={index}>
+                <Row>
+                  <Col>
+                    <Form.Group className="mb-3" controlId={`event-${index}`}>
+                      <Form.Control
+                        type="text"
+                        style={{ width: '100%' }}
+                        placeholder={`Event ${index + 1}`}
+                        value={programme.event}
+                        onChange={(e) => {
+                          const updatedProgrammeDetails = [...programmeDetails];
+                          updatedProgrammeDetails[index].event = e.target.value;
+                          setProgrammeDetails(updatedProgrammeDetails);
+                        }}
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Group className="mb-3" controlId={`description-${index}`}>
+                      <Form.Control
+                        as="textarea"
+                        rows={3}
+                        placeholder="Description"
+                        style={{ width: '100%' }}
+                        value={programme.description}
+                        onChange={(e) => {
+                          const updatedProgrammeDetails = [...programmeDetails];
+                          updatedProgrammeDetails[index].description = e.target.value;
+                          setProgrammeDetails(updatedProgrammeDetails);
+                        }}
+                      />
+                    </Form.Group>
+                  </Col>
+
+                </Row>
+                <Row>
+                  <Col md={12} className='w-100'>
+                    <Button
+                      variant="outline-secondary"
+                      className='w-100 mb-2'
+                      onClick={() => removeEventDetails(index)}
+                    >
+                      Close
+                    </Button>
+                  </Col>
+            
+                </Row>
+              </div>
+            ))
+   
+          }
+                  */}
+            <Button variant="outline-secondary" onClick={addEventDetails}
+            className='w-100'>
+              Add Event Details
+            </Button>
+   
+        </Form>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer className="justify-content-center">
+        <Button variant="secondary" onClick={handleCloseEditModal} className="flex-grow-1">
+          Close
+        </Button>
+        <Button type='button' variant="primary" className="flex-grow-1" onClick={editPackage}>
+          Edit Package
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+
+export default EditPlanModal;
