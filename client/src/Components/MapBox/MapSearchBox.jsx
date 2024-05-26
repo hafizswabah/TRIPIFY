1c999a309af337c4d37c009ba3ec84da0cac8b13
hafizswabah <swabah.a.avd121@gmail.com>
2024-05-26 17:15:49 +0530

    add new feture edit the details of activities

--- client/src/Components/MapBox/MapSearchBox.jsx
+++ client/src/Components/MapBox/MapSearchBox.jsx
@@ -1,68 +1,75 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import mapboxAPI from './MapBoxApi';
 import { BiCurrentLocation } from "react-icons/bi";
 
-function MapSearchBox({setDestination}) {
-    const [searchValue, setSearchValue] = useState('');
+function MapSearchBox({ setDestination, defaultLocation }) {
+    const [searchValue, setSearchValue] = useState(defaultLocation || '');
     const [suggestions, setSuggestions] = useState([]);
 
-    const handleSearchChange = (e) => {
-    const { value } = e.target;
-    setSearchValue(value);
-    fetchSuggestions(value);
-  };
-
-  setDestination(searchValue)
-  const fetchSuggestions = async (value) => {
-    try {
-        const url = `/geocoding/v5/mapbox.places/${encodeURIComponent(value)}.json`;
-        const response = await mapboxAPI.get(url);
-      const suggestions = response.data.features.map((feature) => feature.place_name);
-      setSuggestions(suggestions);
-    } catch (error) {
-      console.error('Error fetching suggestions:', error);
-    }
-  };
-
-  const getCurrentLocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
-  };
-
-  const onSuccess = (position) => {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-
-    const accessToken = 'pk.eyJ1Ijoic2hpamFzMDkiLCJhIjoiY2xpaXUyZHQzMDFzeDNlcGEwbHd6ejJmOCJ9.TZzIUmMeUTVSKfdqqSWgWg';
-    const geocodingUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${accessToken}`;
-
-    fetch(geocodingUrl)
-      .then(response => response.json())
-      .then(data => {
-        if (data.features && data.features.length > 0) {
-          const placeName = data.features[0].place_name;
-          setSearchValue(placeName)
-          console.log(placeName);
+    useEffect(() => {
+        if (defaultLocation) {
+            setSearchValue(defaultLocation);
         }
-      })
-      .catch(error => {
-        console.log("Error occurred during geocoding:", error);
-      });
-  };
+    }, [defaultLocation]);
 
-  const onError = (error) => {
-    console.log("Error occurred during geolocation:", error);
-  };
-  
+    const handleSearchChange = (e) => {
+        const { value } = e.target;
+        setSearchValue(value);
+        fetchSuggestions(value);
+        setDestination(value);
+    };
+
+    const fetchSuggestions = async (value) => {
+        try {
+            const url = `/geocoding/v5/mapbox.places/${encodeURIComponent(value)}.json`;
+            const response = await mapboxAPI.get(url);
+            const suggestions = response.data.features.map((feature) => feature.place_name);
+            setSuggestions(suggestions);
+        } catch (error) {
+            console.error('Error fetching suggestions:', error);
+        }
+    };
+
+    const getCurrentLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+        }
+    };
+
+    const onSuccess = (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        const accessToken = 'pk.eyJ1Ijoic2hpamFzMDkiLCJhIjoiY2xpaXUyZHQzMDFzeDNlcGEwbHd6ejJmOCJ9.TZzIUmMeUTVSKfdqqSWgWg';
+        const geocodingUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${accessToken}`;
+
+        fetch(geocodingUrl)
+            .then(response => response.json())
+            .then(data => {
+                if (data.features && data.features.length > 0) {
+                    const placeName = data.features[0].place_name;
+                    setSearchValue(placeName);
+                    setDestination(placeName);
+                    console.log(placeName);
+                }
+            })
+            .catch(error => {
+                console.log("Error occurred during geocoding:", error);
+            });
+    };
+
+    const onError = (error) => {
+        console.log("Error occurred during geolocation:", error);
+    };
 
     const handleSuggestionClick = (suggestion) => {
-    setSearchValue(suggestion);
-    setSuggestions([]);
-  };
-   
+        setSearchValue(suggestion);
+        setDestination(suggestion);
+        setSuggestions([]);
+    };
+
     return (
         <div>
             <fieldset className="username">
@@ -72,7 +79,7 @@ function MapSearchBox({setDestination}) {
                         placeholder="Location"
                         value={searchValue}
                         onChange={handleSearchChange}
-                        style={{width:"100%",borderRadius:"3px",border: '1px solid rgb(207 207 207)'}}
+                        style={{ width: "100%", borderRadius: "3px", border: '1px solid rgb(207 207 207)' }}
                     />
                     {suggestions.length > 0 && (
                         <div className="suggestion-box">
@@ -88,13 +95,13 @@ function MapSearchBox({setDestination}) {
                         </div>
                     )}
                 </div>
-         <div className="current-location" onClick={getCurrentLocation} >
-          <span className='me-2' style={{fontSize:'15px',fontWeight:'300'}}>choose current Location</span>
-          <BiCurrentLocation/>
-          </div>
+                <div className="current-location" onClick={getCurrentLocation} >
+                    <span className='me-2' style={{ fontSize: '15px', fontWeight: '300' }}>Choose Current Location</span>
+                    <BiCurrentLocation />
+                </div>
             </fieldset>
         </div>
-    )
+    );
 }
 
-export default MapSearchBox
+export default MapSearchBox;
--- client/src/modal/EditPlanModal.jsx
+++ client/src/modal/EditPlanModal.jsx
@@ -1,44 +1,31 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
-import formatDate from '../helper/formatDate';
 import MapSearchBox from '../Components/MapBox/MapSearchBox';
 
-const EditPlanModal = ({ showEditModal, handleCloseEditModal, editPlan, handlePackageAdded }) => {
+// Utility function to format date to YYYY-MM-DD
+const formatToISODate = (dateString) => {
+  const [day, month, year] = dateString.split('/');
+  return `${year}-${month}-${day}`;
+};
 
-  const [editPlanDetails, setEditPlanDetails] = useState(null)
+const EditPlanModal = ({ showEditModal, handleCloseEditModal, editPlan, handlePackageAdded }) => {
+  const defaultDate = formatToISODate('14/02/2024');
+  const [editPlanDetails, setEditPlanDetails] = useState(null);
   const [location, setLocation] = useState('');
+  const [date, setDate] = useState(defaultDate);
 
   useEffect(() => {
     if (editPlan) {
-      setEditPlanDetails(editPlan)
+      setEditPlanDetails(editPlan);
+      setDate(editPlan.date ? editPlan.date.split('T')[0] : defaultDate);
+      setLocation(editPlan.location);
     }
-  }, editPlan)
+  }, [editPlan]);
 
-
-  async function editPackage(e) {
-    //   e.preventDefault();
-    // const formData = new FormData();
-    // formData.append('mainImage', mainImage);
-    // for (let i = 0; i < subImages.length; i++) {
-    //     formData.append('subImages', subImages[i]);
-    // }
-    // const packageData = {
-    //   ...state, 
-    // };
-
-    // formData.append('state', JSON.stringify(packageData));
-    // const {data} = await axios.post("/agency/edit-packages", formData, {
-    //     headers: {
-    //         'Content-Type': 'multipart/form-data',
-    //     },
-    // });
-    // if(!data.err){
-    //   handleCloseEditModal()
-    //   handlePackageAdded ()
-    // }
-  }
-  console.log(editPlanDetails, "state");
+  const editPackage = async (e) => {
+    // Your edit package logic here
+  };
 
   return (
     <Modal show={showEditModal} onHide={handleCloseEditModal} centered dialogClassName="modal-md">
@@ -47,159 +34,145 @@ const EditPlanModal = ({ showEditModal, handleCloseEditModal, editPlan, handlePa
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form>
-            <Row>
-              <Col>
-                <Form.Group className="mb-3" controlId="slots">
-                  <Form.Control type="text" style={{ width: '100%' }} placeholder='Programme Name' value={editPlanDetails?.name} onChange={(e) => { setEditPlanDetails({ ...editPlanDetails, name: e.target.value }) }} />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row>
-              <Form.Group className="mb-3" controlId="plans">
-                <Form.Select className="form-select-sm" placeholder='Type' style={{ width: '100%' }} value={editPlanDetails?.category} onChange={(e) => { setEditPlanDetails({ ...editPlanDetails, category: e.target.value }) }}>
-                  <option value="Dj Night">Dj Night</option>
-                  <option value="Desert">Desert Safari</option>
-                  <option value="Sky Dive">Sky Dive</option>
-                  <option value="Sea Drive">Sea Drive</option>
-                  <option value="Trucking">Trucking</option>
-                  <option value="Camp fire">camp Fire</option>
-                </Form.Select>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="slots">
+                <Form.Control 
+                  type="text" 
+                  style={{ width: '100%' }} 
+                  placeholder="Programme Name" 
+                  value={editPlanDetails?.name || ''} 
+                  onChange={(e) => setEditPlanDetails({ ...editPlanDetails, name: e.target.value })} 
+                />
               </Form.Group>
-            </Row>
-            <Row>
-              <Col>
-                <MapSearchBox setDestination={setLocation}></MapSearchBox>
-              </Col>
-            </Row>
-
-            <Row>
-              <Col>
-                <Form.Group className="mb-3" controlId="slots">
-                  <Form.Control type="date" style={{ width: '100%' }} placeholder='Date' value={editPlanDetails?.date} onChange={(e) => { setEditPlanDetails({ ...editPlanDetails, date: e.target.value }) }} />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Form.Group className="mb-3" controlId="Time">
-                  <Form.Control type="text" style={{ width: '100%' }} placeholder='Time' value={editPlanDetails?.time} onChange={(e) => { setEditPlanDetails({ ...editPlanDetails, time: e.target.value }) }} />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Form.Group className="mb-3" controlId="slots">
-                  <Form.Control type="number" style={{ width: '100%' }} placeholder='Total Slots' value={editPlanDetails?.totalSlots} onChange={(e) => { setEditPlanDetails({ ...editPlanDetails, totalSlots: e.target.value }) }} />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Form.Group className="mb-3" controlId="slots">
-                  <Form.Control type="number" style={{ width: '100%' }} placeholder='Per Head Cost' value={editPlanDetails?.cost} onChange={(e) => { setEditPlanDetails({ ...editPlanDetails, cost: e.target.value }) }} />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Form.Group className="mb-3" controlId="description">
-                  <Form.Control as="textarea" rows={3} placeholder="Description" style={{ width: '100%' }} value={editPlanDetails?.description} onChange={(e) => { setEditPlanDetails({ ...editPlanDetails, description: e.target.value }) }} />
-                </Form.Group>
-              </Col>
-            </Row>
-
-            <Row>
-              <Col>
-                <Form.Group className="mb-3" controlId="image">
-                  <Form.Label className="form-label-sm">Event Poster Image</Form.Label>
-                  <Form.Control type="file" accept='image/*' style={{ width: '100%' }} onChange={(e) => { setEditPlanDetails({ ...editPlanDetails, mainImage: e.target.files[0] }) }} />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Form.Group className="mb-3" controlId="subImages">
-                  <Form.Label className="form-label-sm">Event Sub Images</Form.Label>
-                  <Form.Control type="file" accept='image/*' multiple style={{ width: '100%' }} onChange={(e) => { setEditPlanDetails({ ...editPlanDetails, subImages: e.target.files }) }} />
-                </Form.Group>
-              </Col>
-            </Row>
-            {/* {
-            programmeDetails.map((programme, index) => (
-              <div key={index}>
-                <Row>
-                  <Col>
-                    <Form.Group className="mb-3" controlId={`event-${index}`}>
-                      <Form.Control
-                        type="text"
-                        style={{ width: '100%' }}
-                        placeholder={`Event ${index + 1}`}
-                        value={programme.event}
-                        onChange={(e) => {
-                          const updatedProgrammeDetails = [...programmeDetails];
-                          updatedProgrammeDetails[index].event = e.target.value;
-                          setProgrammeDetails(updatedProgrammeDetails);
-                        }}
-                      />
-                    </Form.Group>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <Form.Group className="mb-3" controlId={`description-${index}`}>
-                      <Form.Control
-                        as="textarea"
-                        rows={3}
-                        placeholder="Description"
-                        style={{ width: '100%' }}
-                        value={programme.description}
-                        onChange={(e) => {
-                          const updatedProgrammeDetails = [...programmeDetails];
-                          updatedProgrammeDetails[index].description = e.target.value;
-                          setProgrammeDetails(updatedProgrammeDetails);
-                        }}
-                      />
-                    </Form.Group>
-                  </Col>
-
-                </Row>
-                <Row>
-                  <Col md={12} className='w-100'>
-                    <Button
-                      variant="outline-secondary"
-                      className='w-100 mb-2'
-                      onClick={() => removeEventDetails(index)}
-                    >
-                      Close
-                    </Button>
-                  </Col>
-            
-                </Row>
-              </div>
-            ))
-   
-          } */}
-            {/*                   
-            <Button variant="outline-secondary" onClick={addEventDetails}
-            className='w-100'>
-              Add Event Details
-            </Button> */}
-
-          </Form>
+            </Col>
+          </Row>
+          <Row>
+            <Form.Group className="mb-3" controlId="plans">
+              <Form.Select 
+                className="form-select-sm" 
+                style={{ width: '100%' }} 
+                value={editPlanDetails?.category || ''} 
+                onChange={(e) => setEditPlanDetails({ ...editPlanDetails, category: e.target.value })}
+              >
+                <option value="Dj Night">Dj Night</option>
+                <option value="Desert">Desert Safari</option>
+                <option value="Sky Dive">Sky Dive</option>
+                <option value="Sea Drive">Sea Drive</option>
+                <option value="Trucking">Trucking</option>
+                <option value="Camp fire">Camp Fire</option>
+              </Form.Select>
+            </Form.Group>
+          </Row>
+          <Row>
+            <Col>
+              <MapSearchBox setDestination={setLocation} defaultLocation={location} />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="slots">
+                <Form.Control 
+                  type="date" 
+                  style={{ width: '100%' }} 
+                  value={date} 
+                  onChange={(e) => setDate(e.target.value)} 
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="Time">
+                <Form.Control 
+                  type="text" 
+                  style={{ width: '100%' }} 
+                  placeholder="Time" 
+                  value={editPlanDetails?.time || ''} 
+                  onChange={(e) => setEditPlanDetails({ ...editPlanDetails, time: e.target.value })} 
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="slots">
+                <Form.Control 
+                  type="number" 
+                  style={{ width: '100%' }} 
+                  placeholder="Total Slots" 
+                  value={editPlanDetails?.totalSlots || ''} 
+                  onChange={(e) => setEditPlanDetails({ ...editPlanDetails, totalSlots: e.target.value })} 
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="slots">
+                <Form.Control 
+                  type="number" 
+                  style={{ width: '100%' }} 
+                  placeholder="Per Head Cost" 
+                  value={editPlanDetails?.cost || ''} 
+                  onChange={(e) => setEditPlanDetails({ ...editPlanDetails, cost: e.target.value })} 
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="description">
+                <Form.Control 
+                  as="textarea" 
+                  rows={3} 
+                  placeholder="Description" 
+                  style={{ width: '100%' }} 
+                  value={editPlanDetails?.description || ''} 
+                  onChange={(e) => setEditPlanDetails({ ...editPlanDetails, description: e.target.value })} 
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="image">
+                <Form.Label className="form-label-sm">Event Poster Image</Form.Label>
+                <Form.Control 
+                  type="file" 
+                  accept="image/*" 
+                  style={{ width: '100%' }} 
+                  onChange={(e) => setEditPlanDetails({ ...editPlanDetails, mainImage: e.target.files[0] })} 
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="subImages">
+                <Form.Label className="form-label-sm">Event Sub Images</Form.Label>
+                <Form.Control 
+                  type="file" 
+                  accept="image/*" 
+                  multiple 
+                  style={{ width: '100%' }} 
+                  onChange={(e) => setEditPlanDetails({ ...editPlanDetails, subImages: e.target.files })} 
+                />
+              </Form.Group>
+            </Col>
+          </Row>
         </Form>
       </Modal.Body>
       <Modal.Footer className="justify-content-center">
         <Button variant="secondary" onClick={handleCloseEditModal} className="flex-grow-1">
           Close
         </Button>
-        <Button type='button' variant="primary" className="flex-grow-1" onClick={editPackage}>
+        <Button type="button" variant="primary" className="flex-grow-1" onClick={editPackage}>
           Edit Package
         </Button>
       </Modal.Footer>
     </Modal>
   );
 };
-
 
 export default EditPlanModal;
